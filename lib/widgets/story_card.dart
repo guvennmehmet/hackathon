e8c993f2c28fc779b8a01211c8356e061760c890
Mehmet <guvennmehmet37@gmail.com>
2023-10-24 22:59:18 +0300

    feat: created the StoryCard Widget

---
+++ lib/widgets/story_card.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:hackathon/models/story.dart';
+
+class StoryCard extends StatelessWidget {
+  final Story story;
+
+  const StoryCard({super.key, required this.story});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(story.storyTitle),
+        onTap: () {},
+      ),
+    );
+  }
+}
