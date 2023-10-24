15e1b06060af9e9187b36d94eaa0a505789c5073
Mehmet <guvennmehmet37@gmail.com>
2023-10-24 23:15:06 +0300

    feat: updated onTap handling in StoryCard

--- lib/widgets/story_card.dart
+++ lib/widgets/story_card.dart
@@ -11,7 +11,12 @@ class StoryCard extends StatelessWidget {
     return Card(
       child: ListTile(
         title: Text(story.storyTitle),
-        onTap: () {},
+        onTap: () {
+          Navigator.of(context).pushNamed(
+            '/story-detail',
+            arguments: story,
+          );
+        },
       ),
     );
   }
