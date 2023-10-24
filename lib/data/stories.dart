2e67141f8fd3a38bcea8c001066807b9c78f901c
Mehmet <guvennmehmet37@gmail.com>
2023-10-24 22:55:56 +0300

    feat: created stories.dart file and added sample stories

---
+++ lib/data/stories.dart
@@ -0,0 +1,36 @@
+import 'package:hackathon/models/story.dart';
+
+List<Story> sampleStories = [
+  Story(
+    storyTitle: "Kayıp Yıldız",
+    storyContent: "Bir zamanlar, küçük bir kasabada yaşayan bir çocuk vardı. Her gece yıldızları izler, en parlak yıldıza dileklerini gönderirdi. Bir gece, o yıldız kayboldu. Çocuk üzüldü, ama ardından kaybolan yıldızın ışığının içinde olduğunu keşfetti. Herkesin içinde bir yıldızı vardır, sadece onu keşfetmeliyiz.",
+  ),
+  Story(
+    storyTitle: "Kırmızı Balon Macerası",
+    storyContent: "Küçük Emily, doğum gününde kırmızı bir balon aldı. Rüzgarın onu götürdüğünü gördü ve balonu takip etmeye karar verdi. Maceraperest bir yolculuk başladı. Ormanda kayboldu, deniz kenarında keyifli vakit geçirdi ve sonunda balonunun yere indiği yerde yeni arkadaşlar edindi.",
+  ),
+  Story(
+    storyTitle: "Zaman Tüneli",
+    storyContent: "Bilim adamı Profesör Smith, zaman tüneli keşfetti. Tünelin içine girdiğinde, geçmişte ve gelecekte yolculuk yapabildi. Ancak her yolculuk sonunda bir şeyler değişiyordu. Profesör, zamanın doğruca değiştirilmemesi gerektiğini öğrendi ve tüneli sadece öğrenmek ve anlamak için kullanmaya başladı.",
+  ),
+  Story(
+    storyTitle: "Kayıp Anahtar Macerası",
+    storyContent: "Küçük Tommy, dedesinin çatısındaki eski bir kutunun içinde eski bir anahtar buldu. Anahtarın nereye açıldığını keşfetmek için gizemli bir yolculuğa çıktı. Her kapıyı denedi, ancak sonunda anahtarın ailesinin kalbini açtığını keşfetti.",
+  ),
+  Story(
+    storyTitle: "Gizli Ormanın Sırrı",
+    storyContent: "Lucy ve Jake, yaz tatilini ormanda kamp yaparak geçirmeye karar verdiler. Ormanda dolaşırken, gizli bir mağara keşfettiler. Mağara içinde, eski bir defter ve haritalar buldular. Bu defter ve haritalar, ormanın derinliklerinde saklanan bir hazineyi aramalarına neden oldu.",
+  ),
+  Story(
+    storyTitle: "Sihirli Kütüphane",
+    storyContent: "Marie, eski bir kütüphanenin derin köşelerinde kaybolmuş bir kitap buldu. Kitabı açtığında, içine çekildi ve sihirli bir dünyaya ulaştı. Bu dünyada, masallar gerçekti ve maceralar onu bekliyordu. Marie, kitabın sayfalarını çevirdikçe farklı hikayelere yolculuk yapmaya başladı.",
+  ),
+  Story(
+    storyTitle: "Uzaylı Misafir",
+    storyContent: "Bir gece, küçük Tim'in penceresinden parlak bir ışık geldi. Tim, bahçesinde garip bir uzaylı gemisi buldu. Uzaylı dost canlısıydı ve dünyayı keşfetmeye geldi. Tim ve uzaylı, birlikte macera dolu bir gün geçirdiler ve dostlukları dünyaya barış getirdi.",
+  ),
+  Story(
+    storyTitle: "Rüyaların Diyarı",
+    storyContent: "Kız kardeşler Mia ve Lily, aynı rüyayı paylaşıyordu. Bu rüyada, onları sihirli bir dünyaya götüren bir kapı vardı. Birlikte bu dünyayı keşfetmeye karar verdiler. Rüyaların diyarında, masallar gerçekti ve hayal güçleri gerçekleşiyordu. Kardeşler, bu sihirli dünyada birçok macera yaşadılar.",
+  ),
+];
