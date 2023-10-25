27c4b021b629076a50dbd1cc826026aaa4fccabe
Mehmet <guvennmehmet37@gmail.com>
2023-10-25 23:39:46 +0300

    feat: added Color Palette and Story Image to StoryDetailPage

--- lib/pages/story_detail_page.dart
+++ lib/pages/story_detail_page.dart
@@ -1,21 +1,63 @@
 import 'package:flutter/material.dart';
 import 'package:hackathon/models/story.dart';
+import 'package:palette_generator/palette_generator.dart';
 
-class StoryDetailPage extends StatelessWidget {
+class StoryDetailPage extends StatefulWidget {
   final Story story;
 
-  const StoryDetailPage({super.key, required this.story});
+  const StoryDetailPage({Key? key, required this.story}) : super(key: key);
+
+  @override
+  State<StoryDetailPage> createState() => _StoryDetailPageState();
+}
+
+class _StoryDetailPageState extends State<StoryDetailPage> {
+  Color appBarColor = Colors.transparent;
+  late PaletteGenerator _generator;
+
+  @override
+  void initState() {
+    super.initState();
+    findAppBarColor();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(story.storyTitle),
+        title: Text(widget.story.storyTitle),
+        backgroundColor: appBarColor,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Text(story.storyContent),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 16, left: 16, right: 16),
+              child: SizedBox(
+                width: double.infinity,
+                child: Image.asset(
+                  'assets/images/${widget.story.storyImage}',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Text(
+                widget.story.storyContent,
+                style: const TextStyle(fontSize: 16),
+              ),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  void findAppBarColor() async {
+    _generator = await PaletteGenerator.fromImageProvider(
+        AssetImage('assets/images/${widget.story.storyImage}'));
+    appBarColor = _generator.vibrantColor!.color;
+    setState(() {});
   }
 }
