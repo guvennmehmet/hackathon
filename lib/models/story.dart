5830ec016cd9dfa7f345925eed552e63cf75c9f7
Mehmet <guvennmehmet37@gmail.com>
2023-10-25 23:37:29 +0300

    feat: expanded Story model to include a story image

--- lib/models/story.dart
+++ lib/models/story.dart
@@ -1,6 +1,7 @@
 class Story {
   String storyTitle;
   String storyContent;
+  String storyImage;
 
-  Story({required this.storyTitle, required this.storyContent});
+  Story({required this.storyTitle, required this.storyContent, required this.storyImage});
 }
