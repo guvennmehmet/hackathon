f343820c149651369da86359383de428cb63aeca
Mehmet <guvennmehmet37@gmail.com>
2023-10-29 19:31:39 +0300

    feat: updated stories with storyID

--- lib/data/stories.dart
+++ lib/data/stories.dart
@@ -2,6 +2,7 @@ import 'package:hackathon/models/story.dart';
 
 List<Story> sampleStories = [
   Story(
+    storyID: 0,
     storyTitle: "Kayıp Yıldız",
     storyImage: "kayip_yildiz.jpg",
     storyContent: "Bir zamanlar, küçük bir kasabada yaşayan bir çocuk vardı. Her gece yıldızları izler, en parlak yıldıza dileklerini gönderirdi. Bir gece, o yıldız kayboldu. Çocuk üzüldü, ama ardından kaybolan yıldızın ışığının içinde olduğunu keşfetti. Herkesin içinde bir yıldızı vardır, sadece onu keşfetmeliyiz."
@@ -14,36 +15,43 @@ List<Story> sampleStories = [
         "Bir zamanlar, küçük bir kasabada yaşayan bir çocuk vardı. Her gece yıldızları izler, en parlak yıldıza dileklerini gönderirdi. Bir gece, o yıldız kayboldu. Çocuk üzüldü, ama ardından kaybolan yıldızın ışığının içinde olduğunu keşfetti. Herkesin içinde bir yıldızı vardır, sadece onu keşfetmeliyiz.",
   ),
   Story(
+    storyID: 1,
     storyTitle: "Kırmızı Balon Macerası",
     storyImage: "kirmizi_balon_macerasi.jpg",
     storyContent: "Küçük Emily, doğum gününde kırmızı bir balon aldı. Rüzgarın onu götürdüğünü gördü ve balonu takip etmeye karar verdi. Maceraperest bir yolculuk başladı. Ormanda kayboldu, deniz kenarında keyifli vakit geçirdi ve sonunda balonunun yere indiği yerde yeni arkadaşlar edindi.",
   ),
   Story(
+    storyID: 2,
     storyTitle: "Zaman Tüneli",
     storyImage: "zaman_tuneli.jpg",
     storyContent: "Bilim adamı Profesör Smith, zaman tüneli keşfetti. Tünelin içine girdiğinde, geçmişte ve gelecekte yolculuk yapabildi. Ancak her yolculuk sonunda bir şeyler değişiyordu. Profesör, zamanın doğruca değiştirilmemesi gerektiğini öğrendi ve tüneli sadece öğrenmek ve anlamak için kullanmaya başladı.",
   ),
   Story(
+    storyID: 3,
     storyTitle: "Kayıp Anahtar Macerası",
     storyImage: "kayip_anahtar_macerasi.jpg",
     storyContent: "Küçük Tommy, dedesinin çatısındaki eski bir kutunun içinde eski bir anahtar buldu. Anahtarın nereye açıldığını keşfetmek için gizemli bir yolculuğa çıktı. Her kapıyı denedi, ancak sonunda anahtarın ailesinin kalbini açtığını keşfetti.",
   ),
   Story(
+    storyID: 4,
     storyTitle: "Gizli Ormanın Sırrı",
     storyImage: "gizli_ormanin_sirri.jpg",
     storyContent: "Lucy ve Jake, yaz tatilini ormanda kamp yaparak geçirmeye karar verdiler. Ormanda dolaşırken, gizli bir mağara keşfettiler. Mağara içinde, eski bir defter ve haritalar buldular. Bu defter ve haritalar, ormanın derinliklerinde saklanan bir hazineyi aramalarına neden oldu.",
   ),
   Story(
+    storyID: 5,
     storyTitle: "Sihirli Kütüphane",
     storyImage: "sihirli_kutuphane.jpg",
     storyContent: "Marie, eski bir kütüphanenin derin köşelerinde kaybolmuş bir kitap buldu. Kitabı açtığında, içine çekildi ve sihirli bir dünyaya ulaştı. Bu dünyada, masallar gerçekti ve maceralar onu bekliyordu. Marie, kitabın sayfalarını çevirdikçe farklı hikayelere yolculuk yapmaya başladı.",
   ),
   Story(
+    storyID: 6,
     storyTitle: "Uzaylı Misafir",
     storyImage: "uzayli_misafir.jpg",
     storyContent: "Bir gece, küçük Tim'in penceresinden parlak bir ışık geldi. Tim, bahçesinde garip bir uzaylı gemisi buldu. Uzaylı dost canlısıydı ve dünyayı keşfetmeye geldi. Tim ve uzaylı, birlikte macera dolu bir gün geçirdiler ve dostlukları dünyaya barış getirdi.",
   ),
   Story(
+    storyID: 7,
     storyTitle: "Rüyaların Diyarı",
     storyImage: "ruyalarin_diyari.jpg",
     storyContent: "Kız kardeşler Mia ve Lily, aynı rüyayı paylaşıyordu. Bu rüyada, onları sihirli bir dünyaya götüren bir kapı vardı. Birlikte bu dünyayı keşfetmeye karar verdiler. Rüyaların diyarında, masallar gerçekti ve hayal güçleri gerçekleşiyordu. Kardeşler, bu sihirli dünyada birçok macera yaşadılar.",
