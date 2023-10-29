62003ed7e361a5a43f34278fc18334f84393678f
Mehmet <guvennmehmet37@gmail.com>
2023-10-29 19:33:12 +0300

    feat: added scroll back to top button to Stories Page

--- lib/pages/stories_page.dart
+++ lib/pages/stories_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hackathon/components/scroll_back_to_top_button.dart';
 import 'package:hackathon/data/stories.dart';
 import 'package:hackathon/widgets/story_card.dart';
 
@@ -7,16 +8,23 @@ class StoriesPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final ScrollController scrollController = ScrollController();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Hikayeler"),
       ),
       body: ListView.builder(
+        controller: scrollController,
         itemCount: sampleStories.length,
         itemBuilder: (context, index) {
-          return StoryCard(story: sampleStories[index]);
+          return Padding(
+            padding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
+            child: StoryCard(story: sampleStories[index]),
+          );
         },
       ),
+      floatingActionButton: BackToTopButton(controller: scrollController),
     );
   }
 }
