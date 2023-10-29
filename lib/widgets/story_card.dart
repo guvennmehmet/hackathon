109029bde17c0e93c4a07f7178b5685ec5e9fc4e
Mehmet <guvennmehmet37@gmail.com>
2023-10-29 19:32:10 +0300

    feat: added navigation to Story Card

--- lib/widgets/story_card.dart
+++ lib/widgets/story_card.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hackathon/models/story.dart';
+import 'package:hackathon/pages/story_detail_page.dart';
+import 'package:hive/hive.dart';
 
 class StoryCard extends StatelessWidget {
   final Story story;
@@ -12,22 +14,36 @@ class StoryCard extends StatelessWidget {
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10.0),
         side: const BorderSide(
-          color: Colors.pink,
+          color: Colors.grey,
           width: 2.0,
         ),
       ),
       child: ListTile(
-        title: Text(story.storyTitle),
-        onTap: () {
-          Navigator.of(context).pushNamed(
-            '/story-detail',
-            arguments: story,
-          );
-        },
-        trailing: const Icon(
-            Icons.arrow_forward_ios,
-          color: Colors.purple,
+        title: Text(
+          story.storyTitle,
+          style: const TextStyle(fontWeight: FontWeight.bold),
         ),
+        onTap: () {
+          final scrollController = ScrollController();
+
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => StoryDetailPage(
+                story: story,
+                scrollController: scrollController,
+              ),
+            ),
+          ).then((updatedStory) {
+            if (updatedStory != null) {
+              if (!updatedStory.isFavorite) {
+                final favoriteBox = Hive.box<Story>('favoriteStories');
+                favoriteBox.delete(updatedStory.key);
+              }
+            }
+          });
+        },
+        trailing: const Icon(Icons.arrow_forward_ios),
       ),
     );
   }
