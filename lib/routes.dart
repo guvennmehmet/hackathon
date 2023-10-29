3a4842346eb3333987625d0dff71ee9c0f10e1a2
Mehmet <guvennmehmet37@gmail.com>
2023-10-29 19:33:49 +0300

    feat: added favorite story detail navigation

--- lib/routes.dart
+++ lib/routes.dart
@@ -4,13 +4,22 @@ import 'package:hackathon/pages/stories_page.dart';
 import 'package:hackathon/pages/story_detail_page.dart';
 
 class Routes {
+  static late ScrollController scrollController;
+
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case '/':
         return MaterialPageRoute(builder: (_) => const StoriesPage());
       case '/story-detail':
         final args = settings.arguments as Story;
-        return MaterialPageRoute(builder: (_) => StoryDetailPage(story: args));
+        return MaterialPageRoute(
+            builder: (_) => StoryDetailPage(
+                story: args, scrollController: scrollController));
+      case '/favorite-story-detail':
+        final args = settings.arguments as Story; // Assuming you have a FavoriteStory model
+        return MaterialPageRoute(
+            builder: (_) => StoryDetailPage(
+                story: args, scrollController: scrollController));
       default:
         return MaterialPageRoute(builder: (_) => const StoriesPage());
     }
