4f215ca0243423581cd1276ad6c6088da23ff058
Ahmet Tarık Kazan <ahmettarkazan@gmail.com>
2023-10-30 21:33:16 +0300

    refactor: ai story detail page added to routes

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hackathon/models/story.dart';
+import 'package:hackathon/pages/ai_story_detail.dart';
 import 'package:hackathon/pages/stories_page.dart';
 import 'package:hackathon/pages/story_detail_page.dart';
 
@@ -20,6 +21,12 @@ class Routes {
         return MaterialPageRoute(
             builder: (_) => StoryDetailPage(
                 story: args, scrollController: scrollController));
+      case '/ai-story-detail':
+        final args = settings.arguments as Map<String, String>; // Map ile argümanı al
+        return MaterialPageRoute(
+            builder: (_) => AiStoryDetailPage(
+                subject: args['subject'] as String,
+                character: args['character'] as String));
       default:
         return MaterialPageRoute(builder: (_) => const StoriesPage());
     }
