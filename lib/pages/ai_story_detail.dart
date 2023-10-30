577eb061b9f887d6c7338b30a6741a4dffe3b755
Ahmet Tarık Kazan <ahmettarkazan@gmail.com>
2023-10-30 22:30:24 +0300

    feature: service implemented to ai story detail page

--- lib/pages/ai_story_detail.dart
+++ lib/pages/ai_story_detail.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hackathon/service/ai_story_service.dart';
+
 
 class AiStoryDetailPage extends StatefulWidget {
   final String subject;
@@ -10,9 +12,53 @@ class AiStoryDetailPage extends StatefulWidget {
 }
 
 class _AiStoryDetailPageState extends State<AiStoryDetailPage> {
+  String generatedStoryUI = "";
+
+  Future<void> generateStory() async {
+    final subject = widget.subject;
+    final character = widget.character;
+
+    final story = "$subject ile ilgili $character'in ana karakter olduğu 100 kelimelik bir masal anlat";
+
+    final generatedStory= await AIStoryService.generateStory(story);
+
+    setState(() {
+      generatedStoryUI = generatedStory;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("${widget.subject} ${widget.character}"),);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("AI Hikaye Oluşturucu"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ElevatedButton(
+              onPressed: generateStory, // Hikaye oluştur metodu
+              child: const Text("Hikaye Oluştur"),
+            ),
+            const SizedBox(height: 16),
+            const Text(
+              "Oluşturulan Hikaye:",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Text(
+                  generatedStoryUI,
+                  style: const TextStyle(fontSize: 16),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
