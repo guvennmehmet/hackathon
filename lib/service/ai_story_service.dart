e3aa23028f092a97c5959227271373fa47874fb9
Mehmet <guvennmehmet37@gmail.com>
2023-10-31 22:34:28 +0300

    refactor: updated AIStoryService

--- lib/service/ai_story_service.dart
+++ lib/service/ai_story_service.dart
@@ -3,11 +3,9 @@ import 'dart:convert';
 
 class AIStoryService {
   static const String apiKey = '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';
-  static const String model = 'davinci';
+  static const String model = 'gpt-3.5-turbo-instruct';
 
-  static Future<String> generateStory(String story) async {
-    final prompt = story;
-
+  static Future<String> generateStory(String prompt) async {
     final Map<String, String> headers = {
       'Authorization': 'Bearer $apiKey',
       'Content-Type': 'application/json',
@@ -16,10 +14,15 @@ class AIStoryService {
     final Map<String, dynamic> requestBody = {
       'model': model,
       'prompt': prompt,
-      'max_tokens': 100,
+      'temperature': 0.9,
+      'max_tokens': 500,
+      'top_p': 1,
+      'frequency_penalty': 0,
+      'presence_penalty': 0.6,
+      'stop': [" Human:", " AI:"]
     };
 
-    final Uri uri = Uri.parse('https://api.openai.com/v1/engines/$model/completions');
+    final Uri uri = Uri.parse('https://api.openai.com/v1/completions');
 
     final response = await http.post(
       uri,
@@ -30,9 +33,15 @@ class AIStoryService {
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
       final generatedStory = data['choices'][0]['text'];
-      return generatedStory;
+      final fixedStory = fixCharacterEncoding(generatedStory);
+      print(fixedStory);
+      return fixedStory;
     } else {
       throw Exception('Hikaye oluşturma hatası: ${response.reasonPhrase}');
     }
+  }
+
+  static String fixCharacterEncoding(String text) {
+    return utf8.decode(const Latin1Codec().encode(text));
   }
 }
